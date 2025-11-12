4aa0233b26f832cd1ef8ba70387a83382023165d
vgonzalez2605 <vgonzalez.email@gmail.com>
2025-11-11 22:34:57 -0500

    Update train_eval.py

--- classification/train_eval.py
+++ classification/train_eval.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, accuracy_score
-from classification import build_pipeline
+from utils import build_pipeline
 
 def load_mtsamples():
     """
